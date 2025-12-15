54f10b8c27008094d1132a6517cd155bc4aa321b
Rakshitha K <rakshitha@CLAP805.prodapt.com>
2025-12-15 11:02:27 +0530

    updated code

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from llm import llm_answer
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class QueryRequest(BaseModel):
     question:str
@@ -9,6 +20,61 @@ class QueryRequest(BaseModel):
 class QueryResponse(BaseModel):
     answer:str
 
+@app.post("/ask",response_model=QueryResponse)
+def ask_ques(payload: QueryRequest):
+    
+    questions=payload.question
+    crops = ["wheat", "tomato", "tomatoes", "pepper", "peppers",
+             "maize", "corn", "sugarcane", "rice"]
+
+    intent_keywords = ["harvest", "ready", "readiness", "maturity", "mature",
+                       "ripeness", "ripe", "moisture", "brix", "firmness"]
+
+    is_crop = any(crop in questions for crop in crops)
+    is_harvest_intent = any(k in questions for k in intent_keywords)
+
+    if is_crop and is_harvest_intent:
+        ans = llm_answer(payload.question)
+
+    else:
+        ans="Right now I'm a simple demo. I can answer basic harvest questions about wheat and tomatoes. Try asking, for example: 'When is wheat ready to harvest?' "
+    
+
+    return QueryResponse(answer=ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 @app.post("/ans",response_model=QueryResponse)
 def ask_ques(payload: QueryRequest):
     
@@ -22,7 +88,7 @@ def ask_ques(payload: QueryRequest):
     
 
     return QueryResponse(answer=ans)
-
+'''
 
 
 
